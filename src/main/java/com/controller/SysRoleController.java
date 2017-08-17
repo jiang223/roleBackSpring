7959a24d945072bc71f5c7613b633f85646f2aab
ll <ll123>
2017-08-17 18:00:35 +0800

    1

--- src/main/java/com/controller/SysRoleController.java
+++ src/main/java/com/controller/SysRoleController.java
@@ -1,13 +1,9 @@
 package com.controller;
 
-
-import com.dao.FunDao;
+import com.dao.RoleDao;
 import com.util.ParentController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -17,38 +13,29 @@ import java.util.Map;
  * Created by Administrator on 2017-08-16.
  */
 @Controller
-@RequestMapping("/sysMenu")
-public class SysMenuController extends ParentController {
+@RequestMapping("/sysRole")
+public class SysRoleController extends ParentController {
     @Resource
-    FunDao funDao;
+    RoleDao roleDao;
 
     @RequestMapping(method =RequestMethod.GET)
     @ResponseBody
     public Map get(HttpSession session, @RequestParam Map<String,Object>map)
     {
         try {
-            List<Map> list=funDao.findMenuAll();
+            List<Map> list=roleDao.findPage(map);
             return resultSucess(list);
         }catch (Exception e){
             e.printStackTrace();
             return resultError("失败"+e);
         }
-
     }
     @RequestMapping(method =RequestMethod.POST)
     @ResponseBody
     public Map add(HttpSession session, @RequestBody Map<String,Object>map)
     {
         try {
-            funDao.insertSelective(map);
-            String route;
-            if ("".equals((String)map.get("route"))){
-                 route=map.get("id").toString();
-            }else{
-                 route=(String)map.get("route")+"-"+map.get("id").toString();
-            }
-            map.put("route",route);
-            funDao.updateByPrimaryKeySelective(map);
+            roleDao.insertSelective(map);
             return resultSucess("添加成功");
         }catch (Exception e){
             e.printStackTrace();
@@ -62,7 +49,19 @@ public class SysMenuController extends ParentController {
     public Map update(HttpSession session, @RequestBody Map<String,Object>map)
     {
         try {
-            funDao.updateByPrimaryKeySelective(map);
+            roleDao.updateByPrimaryKeySelective(map);
+            return resultSucess("修改成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            return resultError("失败"+e);
+        }
+
+    }
+    @RequestMapping(value = "/updateFunction" ,method =RequestMethod.PATCH)
+    @ResponseBody
+    public Map updateFunction(HttpSession session, @RequestBody Map<String,Object>map)
+    {
+        try {
             return resultSucess("修改成功");
         }catch (Exception e){
             e.printStackTrace();
@@ -75,7 +74,7 @@ public class SysMenuController extends ParentController {
     public Map delete(HttpSession session, @RequestBody Map<String,Object>map)
     {
         try {
-            funDao.deleteById(map);
+            roleDao.deleteById(map);
             return resultSucess("删除成功");
         }catch (Exception e){
             e.printStackTrace();
--- src/main/java/com/dao/RoleDao.java
+++ src/main/java/com/dao/RoleDao.java
@@ -3,7 +3,13 @@ package com.dao;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public interface RoleDao {
-
+    List<Map> findPage(Map map);
+    void insertSelective(Map map);
+    void updateByPrimaryKeySelective(Map map);
+    void deleteById(Map map);
 }
